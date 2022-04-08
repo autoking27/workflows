1cfe6a50f5a279f352b2c72ed0a449e6a7ce0564
Kenmakhanu <Kennedy.makhanu@gmail.com>
2022-04-08 17:34:04 -0400

    feat: testing feature2

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: "master"
+    branches: "main"
     repositoryUrl: "https://github.com/Kenmakhanu/workflows",
     plugins: [
         '@semantic-release/commit-analyzer',

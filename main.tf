fca26faf9dedc32c3f48eed8585e616f95b38554
Kenmakhanu <73675815+Kenmakhanu@users.noreply.github.com>
2022-04-28 08:10:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,5 @@ terraform {
 provider "aws" {
   region  = var.aws_region
   #profile = "default"
-  shared_credentials_file = "$HOME/.aws/credentials"
+  #shared_credentials_file = "$HOME/.aws/credentials"
 }

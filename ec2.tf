11c77f66653e1e12fe9defa6df95bbb3d27a64e7
Kenmakhanu <Kennedy.makhanu@gmail.com>
2022-04-28 08:26:28 -0400

    another

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ubuntu" {
-  ami = data.aws_ami.ubuntu.id
+  ami = ami-0e5b6b6a9f3db6db8 #data.aws_ami.ubuntu.id
   instance_type = var.my_instance_type
   #user_data = file("${path.module}/ansible-install-ubuntu.sh")
   key_name = var.my_key
